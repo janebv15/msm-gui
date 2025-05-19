367bb890b2b42bfe85944ce4bf41cbaa5395c493
janebv15 <jvaughn0@chicagobooth.edu>
2025-05-19 00:07:08 +0000

    actors and director

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,53 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+  def create
+  
+         the_name  = params.fetch("query_name")
+     the_dob   = params.fetch("query_dob")
+     the_bio   = params.fetch("bio_box")
+     the_image = params.fetch("query_image")
+
+     new_actor = Actor.new
+     new_actor.name  = the_name
+     new_actor.dob   = the_dob
+     new_actor.bio   = the_bio
+     new_actor.image = the_image
+     new_actor.save
+
+     redirect_to("/actors")
+   end
+
+     def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Actor.where({ :id => the_id })
+
+    the_actor = matching_records.at(0)
+
+    the_actor.destroy
+
+    redirect_to("/actors")
+
+   end
+
+    def update
+      the_id = params.fetch("the_id")
+
+      matching_records = Actor.where({ :id => the_id})
+      the_director = matching_records.at(0)
+
+            
+
+     the_actor.name  = params.fetch("the_actor")
+     the_actor.dob   = params.fetch("query_dob")
+     the_actor.bio   = params.fetch("bio_box")
+     the_actor.image = params.fetch("image_box")
+
+     the_actor.save
+
+
+      redirect_to("/actors/#{the_actor.id}")
+  end
 end
--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -36,4 +36,52 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/eldest" })
   end
+
+   def create
+     the_name  = params.fetch("the_director")
+     the_dob   = params.fetch("query_dob")
+     the_bio   = params.fetch("bio_box")
+     the_image = params.fetch("image_box")
+
+     new_director = Director.new
+     new_director.name  = the_name
+     new_director.dob   = the_dob
+     new_director.bio   = the_bio
+     new_director.image = the_image
+     new_director.save
+
+     redirect_to("/directors")
+   end
+   
+   def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Director.where({ :id => the_id })
+
+    the_director = matching_records.at(0)
+
+    the_director.destroy
+
+    redirect_to("/directors")
+
+   end
+
+    def update
+      the_id = params.fetch("the_id")
+
+      matching_records = Director.where({ :id => the_id})
+      the_director = matching_records.at(0)
+
+            
+
+     the_director.name  = params.fetch("the_director")
+     the_director.dob   = params.fetch("query_dob")
+     the_director.bio   = params.fetch("bio_box")
+     the_director.image = params.fetch("image_box")
+
+     the_director.save
+
+
+      redirect_to("/directors/#{the_director.id}")
+  end
 end
